e1f3e9bbf905184cb04f442adc8857e89d68f443
Mothnep <thomas.penner@student.uva.nl>
2025-07-14 13:40:55 +0200

    Deleted unecessary scripts

--- processor_evaluation.sh
+++
@@ -1,3 +0,0 @@
-arm-linux-gnueabi-g++ -std=c++17 -static -Wall -Wextra -O2 -o iforest_arm algorithms/Iforets_on_OPS-SAT.cpp ../helper/helper_functions.cpp -I../lib/LibIsolationForest/cpp -I..
-
-simulators/gem5/build/ARM/gem5.opt simulators/simulator_configs/gem5/ARM/prog.py
